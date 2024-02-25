d853e8bd26df71d03614bf3157e6e4fb6164cd7b
GenialerGandalf <112351949+GenialerGandalf@users.noreply.github.com>
2024-02-25 20:18:34 +0100

    A_7.5

---
+++ Alpha_Versions/Version-7.5/achieve.py
@@ -0,0 +1,15 @@
+from Texts import Texts
+from rend import *
+
+def Achieves(Ach, screen, x2, achi):
+    if Ach != []:
+        Sach = Ach.sort()
+        print (Sach)
+    screen.fill([0, 0, 0])
+    T = [None, 100, "Achievements", True, 255, 127, 255, screen.get_width()/2, screen.get_height()/16]
+    screen = Texts(T,screen,x2)
+    go = ""
+    go = Return(go)
+    if go == "esc":
+        achi = False
+    return screen, achi
